1616d8b7c60b47bf01fbb0fbc74a1fcef0b90f24
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-03 14:59:30 -0700

    task 12 commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print all possible combination of 2 two-digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int numb1, numb2;
+
+	for (numb1 = 0; numb1 <= 98; numb1++)
+	{
+		for (numb2 = numb1 + 1; numb2 <= 99; numb2++)
+		{
+			putchar((numb1 / 10) + '0');
+			putchar((numb1 % 10) + '0');
+			putchar(' ');
+			putchar((numb2 / 10) + '0');
+			putchar((numb2 % 10) + '0');
+
+			if (numb1 == 98 && numb2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
