7cce36d2f4a52175f90f34658fad5c68cd6d941a
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-07 17:54:18 -0700

    task 13 commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints natural numbers below 1024 that are
+ *
+ *
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			j += i;
+	}
+	printf("%d\n", j);
+	return (0);
+}
