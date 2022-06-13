495ab03107c6981db6ecf6aef1fccd9ed06decfa
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-13 15:11:00 -0700

    task 0 commit

--- main.h
+++ main.h
@@ -1,11 +1,28 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
+int _putchar(char c);
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+void reset_to_98(int *n);
 
-#endif /* main_h */
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *s);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char*s);
+
+#endif /* MAIN_H */
