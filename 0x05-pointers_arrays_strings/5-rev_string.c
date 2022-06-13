5c476155e59320e9c68baf3cecb94882a49b7582
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-13 16:20:55 -0700

    task 5 commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,17 @@
 void rev_string(char *s)
 {
 	int i, j;
+	char k;
 
 	i = 0;
+	j = 0;
+
 	while (s[++j])
 		++i;
-	for (j = i - 1; j >= 0; --j)
-		_putchar(s[j]);
-	_putchar('\n');
+	for (j = i - 1; j >= i / 2; --j)
+	{
+		k = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = k;
+	}
 }
