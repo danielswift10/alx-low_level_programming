f31581fec091e784999f512602e09fa549dee0c2
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-16 11:25:33 -0700

    task 12 commit

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - a function that prints a buffer
+ * @b: buffer to be printed
+ * @size: number of bytes to be printed from the buffer
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%.8x:", i);
+			for (j = i; j < i + 10; j++)
+			{
+				if (j % 2 == 0)
+					printf(" ");
+				if (j < size)
+					printf("%.2x", *(b + j));
+				else
+					printf(" ");
+			}
+			printf(" ");
+			for (k = i; k < i + 10; k++)
+			{
+				if (k >= size)
+					break;
+				if (*(b + 1) < 32 || *(b + 1) > 126)
+					printf("%c", *(b + 1));
+			}
+			printf("\n");
+		}
+	}
+}
