971ee586465d877233aec36d55d488132f98cd75
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-15 23:54:04 -0700

    task 0 commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that appends the src string to the dest string,
+ * overwriting the terminating null byte \0 at the end of the dest, and
+ * then adds a terminating null byte
+ * @dest: pointer destination
+ * @src: source string to be appendded to @dest
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i++]; j++)
+		;
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
