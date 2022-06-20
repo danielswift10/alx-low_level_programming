d59af72452e5bc9ae678bfd319ba921b49bcddcf
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-20 06:40:00 -0700

    task 6 commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard
+ * @a: an array to be printed
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		for (j = 0; j < 8; ++j)
+			_putchar(a[i][j]);
+		_putchar('\n');
+		++i;
+	}
+}
