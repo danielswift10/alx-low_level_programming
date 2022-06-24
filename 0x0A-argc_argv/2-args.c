f1b0003569b72cf182c543db9ee3bd24f89837b4
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-24 10:35:24 -0700

    task 2 commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
-#include
+#include <stdio.h>
+
+/**
+ * main - a function that prints all arguments it receives
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; ++i)
+		printf("%s\n", argv[i]);
+	return (0);
+}
