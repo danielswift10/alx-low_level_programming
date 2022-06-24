ebb6dc400a8a8da26a67050cbc6803052417098e
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-24 13:10:19 -0700

    task 1 commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,11 @@
 /**
  * main - a function that prints the number of arguments passed to it
  * @argc: number of arguments supplied to the program
- * @argc: an array of pointers to the arguments
+ * @argv: an array of pointers to the arguments
  * Return: Always 0
  */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
