c119a40dccdfb3202f1bc7c16767c6b80b9c20e8
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-24 12:43:40 -0700

    task 3 commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 
 /**
  * main - a function that multiplies two numbers
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error\n")
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
