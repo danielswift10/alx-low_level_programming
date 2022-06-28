05473e6c36dbb01ccc053951b62cb3f766a1fff9
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-28 13:03:40 -0700

    task 0 commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 
+	array = malloc(sizeof(char) * size);
+
 	if (size == 0 || array == NULL)
 		return (NULL);
 	for (index = 0; index < size; ++index)
