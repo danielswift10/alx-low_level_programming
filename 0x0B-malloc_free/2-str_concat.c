8a511dd235b0d3ff1a08caacb3531c957d7b8d24
¨Daniel <ojeyomi.daniel@gmail.com>
2022-06-28 13:36:22 -0700

    task 2 commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 	for (i = 0; s1[i] || s2[i]; ++i)
 		++l;
 	concat_str = malloc(sizeof(char) * l);
