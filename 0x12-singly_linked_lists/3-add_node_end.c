ec943c9902562f35fa2c9f288b19aa1f3d847bbb
¨Daniel <ojeyomi.daniel@gmail.com>
2022-07-14 07:47:58 -0700

    task commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end
+ *                of a list_t list.
+ * @head: A pointer the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+	size_t i;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (i = 0; str[i]; ++i)
+		;
+
+	new->len = i;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next)
+		free_list(head->next);
+	free(head->str);
+	free(head);
+}
