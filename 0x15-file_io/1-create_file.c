1fafd69270c0b502df9693bafda3af432af2adbd
¨Daniel <ojeyomi.daniel@gmail.com>
2022-07-25 08:24:21 -0700

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,29 +10,59 @@
  */
 
 int create_file(const char *filename, char *text_content)
+
 {
-	int file, i = 0;
 
-	f (filename == NULL)
-		return (-1);
+		int fd;
 
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (file == -1)
-		return (-1);
+			int nletters;
 
-	while (text_content[i])
-		i++;
+				int rwr;
 
-	if (text_content == NULL)
-	{
-		close(file);
-		return (-1);
-	}
-	else
-	{
-		write(file, text_content, i);
-	}
 
-	close(file);
-	return (1);
+
+					if (!filename)
+
+								return (-1);
+
+
+
+						fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+
+
+							if (fd == -1)
+
+										return (-1);
+
+
+
+								if (!text_content)
+
+											text_content = "";
+
+
+
+									for (nletters = 0; text_content[nletters]; nletters++)
+
+												;
+
+
+
+										rwr = write(fd, text_content, nletters);
+
+
+
+											if (rwr == -1)
+
+														return (-1);
+
+
+
+												close(fd);
+
+
+
+													return (1);
+
 }
