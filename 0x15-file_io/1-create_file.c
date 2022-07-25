939af954683c3e4f6fc468b428736f5a5bcc2772
¨Daniel <ojeyomi.daniel@gmail.com>
2022-07-25 07:57:08 -0700

    task 1 commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (file == -1)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (-1);
+	}
+	else
+	{
+		write(file, text_content, i);
+	}
+
+	close(file);
+	return (1);
+}
