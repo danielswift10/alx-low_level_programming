ded7f9ddb87f07dd599add74fb0eeb2a350a46bc
¨Daniel <ojeyomi.daniel@gmail.com>
2022-07-25 08:00:19 -0700

    task 0 commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, rd, wr;
+	char *buf;
+
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters + 1);
+
+	if (buf == NULL)
+		return (0);
+
+	rd = read(file, buf, letters);
+
+	if (rd == -1)
+		return (0);
+
+	buf[letters] = '\0';
+
+	wr = write(1, buf, rd);
+
+	if (wr == -1)
+		return (0);
+
+	close(file);
+	free(buf);
+
+	return (wr);
+}
