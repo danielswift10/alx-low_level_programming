8bc74de49977c8ffd7b2d53ceba29b30941a4a38
Daniel Ojeyomi <73875730+danielswift10@users.noreply.github.com>
2022-07-25 08:21:48 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,28 +11,30 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
-	f (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
+	if (!text_content)
+		text_content = "";
 
-	if (text_content == NULL)
-	{
-		close(file);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
-	}
-	else
-	{
-		write(file, text_content, i);
-	}
 
-	close(file);
+	close(fd);
+
 	return (1);
 }
